0c96a764189adee116da6fffb4d09340db273806
Ira Levkovich <6346390@mail.ru>
2020-08-08 13:12:11 +0300

    Validation check

--- index.js
+++ index.js
@@ -1,35 +1,75 @@
-const surname = prompt("What is your surname?", "");
-const name = prompt("What is your name?", "");
-const middleName = prompt("What is your middle name?", "");
-const age = prompt("What is your age?", "");
-const ageInDays = 365 * age + Math.floor(age / 4);
+let yuorSurname = null;
+while (true) {
+  const surname = prompt("What is your surname?", "");
+  if (Number(surname) || surname === null || surname === "") {
+    alert("Введите корректное значение");
+  } else {
+    yuorSurname = surname;
+    break;
+  }
+}
+
+let yuorName = null;
+while (true) {
+  const name = prompt("What is your name?", "");
+  if (Number(name) || name === null || name === "") {
+    alert("Введите корректное значение");
+  } else {
+    yuorName = name;
+    break;
+  }
+}
+
+let yuorMiddleName = null;
+while (true) {
+  const middleName = prompt("What is your middlename?", "");
+  if (Number(middleName) || middleName === null || middleName === "") {
+    alert("Введите корректное значение");
+  } else {
+    yuorMiddleName = middleName;
+    break;
+  }
+}
+
+let yourAge = null;
+while (true) {
+  const age = prompt("What is your age?", "");
+  if (!Number(age)) {
+    alert("Введите корректное значение");
+  } else {
+    yourAge = age;
+    break;
+  }
+}
+
+const ageInDays = 365 * yourAge + Math.floor(yourAge / 4);
 const gender = confirm("Are you a man?");
 
 let yourGender = null;
 if (gender) {
-    yourGender = "мужчина";
+  yourGender = "мужчина";
 } else {
-    yourGender = "женщина";
+  yourGender = "женщина";
 }
 
 let pension = null;
 if (gender) {
-    if (age >= 65) {
-        pension = "Вы на пенсии";
-    } else {
-        pension = `На вас еще ${65 - age} лет пахать можно!`;
-    }
+  if (yourAge >= 65) {
+    pension = "Вы на пенсии";
+  } else {
+    pension = `На вас еще ${65 - yourAge} лет пахать можно!`;
+  }
 } else {
-    if (age >= 55) {
-        pension = "Вы на пенсии";
-    } else {
-        pension = `На вас еще ${55 - age} лет пахать можно!`;
-    }
+  if (yourAge >= 55) {
+    pension = "Вы на пенсии";
+  } else {
+    pension = `На вас еще ${55 - yourAge} лет пахать можно!`;
+  }
 }
 
-alert(`Ваше ФИО: ${surname + " " + name + " " + middleName}
-Ваш возраст в годах: ${age}
-Ваш возраст в днях: ${ageInDays}
-Через 5 лет вам будет: ${+age + 5}
-Ваш пол: ${yourGender}
-Вы на пенсии: ${pension}`)
+alert(`Ваше ФИО: ${yuorSurname + " " + yuorName + " " + yuorMiddleName}
+ Ваш возраст в годах: ${yourAge} 
+ Ваш возраст в днях: ${ageInDays}
+ Через 5 лет вам будет: ${+yourAge + 5}
+ Ваш пол: ${yourGender}
+ Вы на пенсии: ${pension}`);
